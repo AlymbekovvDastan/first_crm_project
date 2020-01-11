a2eb12d0af87228b91d949f862e184ea3b6d0965
AlymbekovvDastan <alymbek_uulu_d@iuca.kg>
2020-01-11 15:40:37 +0600

    CRM

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from accounts.views import *
+
+urlpatterns = [
+	path('', home, name='home'),
+	path('products/', products, name='products'),
+	path('customer/<int:pk>/', customer, name='customer'),
+	path('create_order/<int:pk>', createOrder, name='create_order'),	
+	path('update_order/<int:pk>', updateOrder, name='update_order'),
+	path('delete_order/<int:pk>', deleteOrder, name='delete_order'),
+	path('register/', registerPage, name='register'),
+	path('login/', loginPage, name='login'),
+	path('logout/', logoutUser, name='logout'),
+	path('user/', userPage, name='user'),
+	path('account', accountSettings, name='account'),
+]
